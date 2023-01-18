3c1281e7bd2967acaaa77df111a723f4d67f00c4
rahff <raphaelandrey99@gmail.com>
2023-01-18 04:21:48 +0100

    add cart page

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,16 @@ router.get("/login", function(req, res) {
   res.render("login");
 });
 
-router.get('shop', function(req, res) {
-  res.render("partials/shop");
+router.get('/register', function(req, res) {
+  res.render("register");
+});
+
+router.get('/forgot_password', function(req, res) {
+  res.render("forgot");
+});
+
+router.get('/cart', function(req, res) {
+  res.render("cart");
 });
 
 module.exports = router;
